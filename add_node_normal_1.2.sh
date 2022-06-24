064b572878651f9c2e47be92b55cfc56b71370f6
Liloker <55574496+Liloker@users.noreply.github.com>
2022-06-24 11:18:28 +0800

    Update add_node_normal_1.2.sh
    update to 1.4

--- add_node_normal_1.2.sh
+++ add_node_normal_1.2.sh
@@ -1,5 +1,33 @@
 #!/bin/bash 
-#默认运行路径root的home目录
+#默认运行路径root/
+add_boot() #加入开机自启
+{
+rc_ubuntu=`cat /etc/os-release | grep -o '[\"].*[\"]'| grep -o 'Ubuntu 1[6|8]'|wc -l`
+if [ $rc_ubuntu -eq 2 ] #20版本之后无/etc/rc.local
+then
+        echo -e "\033[33m Ubuntu version < 20 \033[0m"
+        echo "/root/node_exporter-1.3.1.linux-amd64/node_exporter">> /etc/rc.d/rc.local
+else #添加服务
+        echo -e "\033[33m Ubuntu version ≥ 20 \033[0m"
+cat>/etc/systemd/system/node_exporter.service<<EOF
+[Unit]
+Description=node_exporter Monitoring System
+Documentation=node_exporter Monitoring System
+
+[Service]
+ExecStart=/root/node_exporter-1.3.1.linux-amd64/node_exporter --web.listen-address=:9100 
+
+[Install]
+WantedBy=multi-user.target
+EOF
+sleep 3;
+systemctl daemon-reload
+systemctl start node_exporter.service
+systemctl status node_exporter.service
+systemctl enable node_exporter.service
+echo -e "\033[33m node_exporter.service has been enabled, now the node_exporter will be started automatically in every reboot \033[0m"
+fi
+}
 
 #下载并解压
 install_fun()
@@ -20,12 +48,14 @@ install_fun()
     #         echo -e "\033[32m[386]  \033[0m"
     # fi
     uname -a
-    echo 'downloading and executing node_exporter......'
-    #wget -c -t0 https://github.com/prometheus/node_exporter/releases/download/v0.18.1/node_exporter-0.18.1.linux-amd64.tar.gz
-
+    echo -e "\033[33m Downloading and executing node_exporter......\033[0m"
+    wget -c -t0 https://github.com/prometheus/node_exporter/releases/download/v0.18.1/node_exporter-0.18.1.linux-amd64.tar.gz
+    #使用个人阿里云：默认对于 Linux 实例，默认在管理员 root 用户的 home 目录下；对于 Windows 实例，默认在 C:\Windows\system32 目录
+    #使用公司内网oss，文件单独设置公共读权限，无链接过期时间300s
+    
     tar -xvzf node_exporter-1.3.1.linux-amd64.tar.gz 
     cd node_exporter-1.3.1.linux-amd64/
-    echo -e "\033[33m download and unzip successfully, now testing if the port of 9100 is occupied or not ...... \033[0m"
+    echo -e "\033[33m Download and unzip successfully, now testing if the port of 9100 is occupied or not ...... \033[0m"
     for(( i=0;i<${#port_array[@]};i++)) do
         time=$(date "+%Y-%m-%d %H:%M:%S")
         port=${port_array[i]};
@@ -33,8 +63,11 @@ install_fun()
         if [ $port_status -lt 1 ]
         then    
             echo -e "\033[32m [port available] $time $port\033[0m"
-            nohup ./node_exporter --web.listen-address 0.0.0.0:$port &  
-
+            #nohup ./node_exporter --web.listen-address 0.0.0.0:$port &  
+            #后台运行，此时可能开启另一进程
+            #sleep 5
+            #写入开机启动服务并立即开启服务
+            add_boot;
             #检查是否部署成功并运行
             echo -e "\033[33m testing if the node_exporter process is working or not by checking port $port \033[0m"
             run_status2_1=`curl -s localhost:${port_array[i]}| grep '/metrics' |wc -l `
@@ -49,8 +82,8 @@ install_fun()
             fi
         else
             echo -e "\033[31m [port occupied] $time $port changing port to 9101...\033[0m"
-            if [$port -e 9101]
-            then echo -e "\033[31m 9100,9101端口均被占用，安装失败\033[0m"
+            if [$port -e 9101] #如果即使换成9101端口，还是被占用，则报错安装失败
+            then echo -e "\033[31m Both 9100 and 9101 are occupied, so I stopped the program \033[0m"
             fi
         fi
     done;
@@ -67,9 +100,9 @@ do
     #port_status=`netstat -nlt|grep ${port_array[i]}|wc -l `
     if [[ $run_status1_1 -lt 1 || $run_status1_2 -lt 1 ]]
     then
-        echo -e "\033[31m [port is not working] $time $port node未处于工作状态 \033[0m"
+        echo -e "\033[31m [port is not working] $time $port node not working \033[0m"
     else
-        echo -e "\033[32m [port is on working] $time $port node处于工作状态 \033[0m"
+        echo -e "\033[32m [port is on working] $time $port node is on working \033[0m"
         curl -s 127.0.0.1:$port
         lsof -i | grep node
         exit 0
@@ -83,28 +116,30 @@ str="node"
 instal_stat=`ls | grep ${str} |wc -l `
 if [ $instal_stat -ge 2 ]
 then #安装目录存在
-    echo -e "\033[32m [installed] /root目录下 已存在 node 文件夹与安装包 在9100端口启动该进程......\033[0m"
+    echo -e "\033[32m [installed] The files of node_exporter have been in /root/ directory, now start the node process at port 9100......\033[0m"
     cd node_exporter-1.3.1.linux-amd64/
     nohup ./node_exporter --web.listen-address 0.0.0.0:9100 &
-    sleep 5 #等1s进程启动
+    sleep 5 #等5s进程启动
     temp1=`lsof -i | grep node | wc -l`
     if [ $temp1 -ge 1 ]
     then #如果用9100端口启动成功
-        echo -e "\033[32m [installed] node_exporter已于9100成功运行\033[0m"
+        echo -e "\033[32m [running] node_exporter is working on port 9100 successfully \033[0m"
         lsof -i | grep node
     else #否则尝试用9101端口启动
         nohup ./node_exporter --web.listen-address 0.0.0.0:9101 &
         temp2=`lsof -i | grep node | wc -l`
         if [ $temp2 -ge 1 ]
         then 
-            echo -e "\033[32m [installed] node_exporter已于9101成功运行\033[0m"
+            echo -e "\033[32m [running] node_exporter is working on port 9101 successfully\033[0m"
             lsof -i | grep node
         else
-            echo -e "\033[31m 其他未知错误 \033[0m"
+            echo -e "\033[31m [error] Unknown error occured !!!  \033[0m"
         fi
     fi
 else #安装目录不存在
-    echo -e "\033[31m [installed] 当前目录下不存在 node 文件夹与安装包，下面启动下载与安装程序...... \033[0m"
+    echo -e "\033[31m [none] Nothing about node_exporter in current directory, now loading program for downloading and setuping...... \033[0m"
     install_fun
 fi
+
+
 
